ce70a280a9005caacaa8e0ea1cba3da991a96329
Aabeeya Salman <git@aabeeya.com>
2017-09-24 09:17:15 -0700

    Filters page with restaurant type filter

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, FiltersViewControllerDelegate {
     
     var businesses: [Business]!
-    
+    //var searchedBusiness: [Business]!
+
+    // create the search bar programatically since you won't be able to drag one onto the navigation bar
+    let searchBar = UISearchBar()
+
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        searchBar.sizeToFit()
+
+        /* the UIViewController comes with a navigationItem property this will automatically be initialized for you if when the view controller is added to a navigation controller's stack you just need to set the titleView to be the search bar */
+        navigationItem.titleView = searchBar
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+
+
+        // use whatever auto layout constrains told you to do
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+
+
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.tableView.reloadData()
+
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -38,22 +61,53 @@ class BusinessesViewController: UIViewController {
          }
          }
          */
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /*
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+
+        cell.business = businesses[indexPath.row]
+
+        return cell
+    }
+
+
      // MARK: - Navigation
      
      // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      // Get the new view controller using segue.destinationViewController.
      // Pass the selected object to the new view controller.
+
+        let navigationController = segue.destination as! UINavigationController
+        let filtersViewController = navigationController.topViewController as! FiltersViewController
+
+        filtersViewController.delegate = self
      }
-     */
-    
+
+    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters filters: [String : AnyObject]) {
+
+        var categories = filters["categories"] as? [String]
+
+        Business.searchWithTerm(term: "Restaurants", sort: nil, categories: categories, deals: nil) { (businesses: [Business]?, error) -> Void in
+
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+
+    }
+
 }
